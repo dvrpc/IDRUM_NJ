6cf05b1fb22cf01b40365e765fc67585cab70e58
Christopher Pollard <crvanpollard@gmail.com>
2020-12-16 12:45:08 -0500

    initial file load
    initial file load

---
+++ assets/js/app.js
@@ -0,0 +1,312 @@
+  var geojson;
+  var TTI, cnty, mask;
+ 
+  var hoveredStateId = null;
+  $('#aboutModal').modal('show');
+  // Target the span elements used in the sidebar
+  var magDisplay = document.getElementById('mag');
+  var shieldDisplay = document.getElementById('shield');
+
+  var layer;
+
+  function toggleLayer(e){
+   //  console.log(e);
+      var layer = e;
+ 
+      map.setFilter('mask', ['==', ['get', 'Det_ID'], e]);
+  //    return layer;
+       if ( layer === 'MON_US422' ) {
+             //         filterDay = ['match', ['get', 'Day'], ['Sat', 'Sun'], false, true];
+         map.setFilter('county2', ['match', ['get', 'name'], ['Berks', 'Chester', 'Delaware', 'Montgomery','Bucks','Philadelphia'], false, true]
+        );
+        } else if ( layer === 'CHE_US422' ) {
+             //         filterDay = ['match', ['get', 'Day'], ['Sat', 'Sun'], false, true];
+         map.setFilter('county2', ['match', ['get', 'name'], ['Berks', 'Chester', 'Delaware', 'Montgomery','Bucks','Philadelphia'], false, true]
+        );
+        } else if ( layer === 'MON_PA100' ) {
+             //         filterDay = ['match', ['get', 'Day'], ['Sat', 'Sun'], false, true];
+         map.setFilter('county2', ['match', ['get', 'name'], ['Berks', 'Chester', 'Delaware', 'Montgomery','Bucks','Philadelphia'], false, true]
+        );
+        } else if ( layer === 'CHE_US001' ) {
+             //         filterDay = ['match', ['get', 'Day'], ['Sat', 'Sun'], false, true];
+         map.setFilter('county2', ['match', ['get', 'name'], ['Cecil', 'Chester', 'Delaware', 'Montgomery','Bucks','Philadelphia'], false, true]
+        );
+          } else if ( layer === 'DEL_I095' ) {
+             //         filterDay = ['match', ['get', 'Day'], ['Sat', 'Sun'], false, true];
+         map.setFilter('county2', ['match', ['get', 'name'], ['New Castle', 'Chester', 'Delaware', 'Montgomery','Bucks','Philadelphia'], false, true]
+        );
+        } else {
+         map.setFilter('county2', ['match', ['get', 'name'], ['Chester', 'Delaware', 'Montgomery','Bucks','Philadelphia'], false, true]);
+        //  map.setFilter('county2',["all",["!=","name","Chester"],["!=","name","Bucks"],["!=","name","Delaware"],["!=","name","Montgomery"],["!=","name","Philadelphia"]
+        //, e]);
+       return layer;
+    }
+  };
+
+ $(document).on("mouseover", ".feature-row", function(e) {
+  // alert("Hello! I am an alert box!!");
+   //     highlight.clearLayers().addLayer(L.circleMarker([$(this).attr("lat"), $(this).attr("lng")], highlightStyle));
+    });
+
+        function signhoverIN (e) {
+        //   var active = document.getElementsByTagName(e);
+        //  document.getElementById("img").src = img.src.replace("_t", "_b");
+        var sign = e ;
+        //    document.getElementsById(e).className += "fancy";
+        document.getElementById(sign).className += " fancy";
+        console.log(e);
+        // document.getElementsByTagName(e).className += "fancy";
+        }
+
+        function signhoverOUT () {
+        }
+
+  mapboxgl.accessToken = 'pk.eyJ1IjoiY3J2YW5wb2xsYXJkIiwiYSI6ImNqMHdvdnd5MTAwMWEycXBocm4zbXVjZm8ifQ.3zjbFccILu6mL7cOTtp40A';
+
+  // This adds the map
+    var map = new mapboxgl.Map({
+        container: 'map', 
+   //    style: 'mapbox://styles/mapbox/light-v9', 
+       // also uncomment line 203 (or near there) admin-bg
+        style:'mapbox://styles/crvanpollard/ck5fpyqti0v971itf7edp2eyd',
+        center: [-75.4, 40.15], 
+        zoom: 8,   
+        attributionControl: false
+    });
+
+    function goHome() {
+      // debugger
+      if (map.loaded()) {
+        var p = map.getPitch();
+     //   console.log(p);
+        if (p > 0) {
+          map.flyTo({
+            center: [-75.4, 40.15], 
+            zoom: 8,
+            speed: 0.1,
+          });
+        }
+      }
+    }
+    // Add zoom and rotation controls to the map.
+    map.addControl(new mapboxgl.NavigationControl(),['top-right']);
+    map.addControl(new mapboxgl.AttributionControl(),'bottom-right');
+
+    // Zoom to Extent
+    document.getElementById('zoomtoregion').addEventListener('click', function () {
+        map.flyTo({
+            center: [-75.4, 40.15], 
+            zoom: 8,
+            speed: 0.5
+        });
+    });
+
+map.on('load', function () {
+           var layers = map.getStyle().layers;
+            // Find the index of the first symbol layer in the map style
+            var firstSymbolId;
+            for (var i = 0; i < layers.length; i++) {
+            if (layers[i].type === 'symbol') {
+            firstSymbolId = layers[i].id;
+            break;
+            }
+            }
+           
+            // Grey Mask
+           map.addLayer({
+              "id": "county2",
+              "type": "fill",
+              "source": {
+                  type: 'vector',
+                  url: 'https://tiles.dvrpc.org/data/dvrpc-municipal.json'
+              },
+              "source-layer": "county",
+              "layout": {},
+              paint: {
+              // 'fill-outline-color': '#f7c59f',
+               'fill-color': 'rgba(0,0,0,0.1)'
+              },
+              "filter": 
+              //["==","dvrpc","Yes"]
+              ["all",["!=","name","Chester"],["!=","name","Bucks"],["!=","name","Delaware"],["!=","name","Montgomery"],["!=","name","Philadelphia"]]
+            });
+
+            map.addSource('cnty', {
+            'type': 'geojson',
+               'data':"https://arcgis.dvrpc.org/portal/rest/services/Boundaries/CountyBoundaries/FeatureServer/0/query?where=co_name+%3D+%27Bucks%27+or+co_name+%3D+%27Chester%27+or+co_name+%3D+%27Delaware%27+or+co_name+%3D+%27Montgomery%27+or+co_name+%3D+%27Philadelphia%27&outFields=*&returnGeometry=true&geometryPrecision=8&outSR=4326&f=geojson"
+            });
+            
+            map.addLayer({
+            'id': 'cnty-fills',
+            'type': 'line',
+            'source': 'cnty',
+            'layout': {},
+            'paint': {
+            'line-color': '#898989'
+            }
+            });
+            
+            map.addLayer({
+            id: 'mask',
+            type: 'fill',
+            source: {
+                  'type': 'geojson',
+                  'data':'https://services1.arcgis.com/LWtWv6q6BJyKidj8/ArcGIS/rest/services/IDRuM/FeatureServer/7/query?where=1%3D1&outFields=*&returnGeometry=true&geometryPrecision=8&outSR=4326&f=geojson'
+            },
+            layout: {
+             "visibility":"visible",
+              },
+            paint: {
+              // 'fill-outline-color': '#f7c59f',
+               'fill-color': 'rgba(0,0,0,0.1)'
+            },
+             filter: 
+                 ["all",["==","Det_ID","PA"]]
+            });
+
+
+        // When the map loads, add the data from the USGS earthquake API as a source
+        map.addSource('earthquakes', {
+          'type': 'geojson',
+          'data': 'https://services1.arcgis.com/LWtWv6q6BJyKidj8/ArcGIS/rest/services/IDRuM/FeatureServer/0/query?where=1%3D1&outFields=*&returnGeometry=true&geometryPrecision=8&outSR=4326&f=pgeojson',
+          'generateId': true // This ensures that all features have unique IDs
+        });
+        
+        map.addSource('route', {
+          'type': 'geojson',
+          'data':" https://services1.arcgis.com/LWtWv6q6BJyKidj8/ArcGIS/rest/services/IDRuM/FeatureServer/3/query?where=1%3D1&outFields=*&returnGeometry=true&geometryPrecision=8&outSR=4326&f=pgeojson",
+          'generateId': true // This ensures that all features have unique IDs
+        });
+
+        map.addLayer({
+          id: "route-viz",
+          type: "line",
+          source: 'route',
+          layout: {
+         //     "visibility":"none",
+              "line-join": "round",
+              "line-cap": "round"
+          },
+          paint: {
+             "line-color": [ 'case',
+            ['boolean', ['feature-state', 'hover'], false],
+             '#FFFF00', '#F5921D'
+            ],
+             "line-width": [ 'case',
+            ['boolean', ['feature-state', 'hover'], false],
+             5, 3
+            ]
+          }
+          //  firstSymbolId
+     //   });
+       //  }
+  }, 'admin-1-boundary-bg');
+
+         map.on("click", function(e) {
+          var features = map.queryRenderedFeatures(e.point, { layers: ["route-viz"] });
+          if (features.length) {
+            window.open('https://www.dvrpc.org/asp/idrum/Online/PDF/PA/PADist06/' + features[0].properties.County +'/'+ features[0].properties.State + features[0].properties.CNTY + features[0].properties.Route +'/'+ features[0].properties.Det_ID +'.pdf', '_blank');
+             }
+        });
+
+        // Add earthquakes as a layer and style it
+        map.addLayer({
+          'id': 'earthquakes-viz',
+          'type': 'circle',
+          'source': 'earthquakes',
+          'paint': {
+            'circle-radius': 3,
+            'circle-stroke-color': '#FF5702',
+            'circle-stroke-width': .3,
+            'circle-color':'#ff4500'
+          }
+        });
+
+        map.addLayer({
+          "id": "clusters-label",
+          "type": "symbol",
+          "source": "earthquakes",
+          "layout": {
+            "text-field": "{Location}",
+            "text-font": ["Montserrat SemiBold", "Open Sans Semibold"],
+            "text-size": 10,
+            "text-anchor": "left",
+            "text-offset":[1,0]
+        //    "text-size": ["interpolate", ["linear"], ["zoom"], 8, 8, 12, 12]
+          },
+           "paint": {
+                "text-color": "#ff7440",
+                "text-halo-color":"#fff",
+                "text-halo-width": 2,
+                "text-halo-blur": 3
+              }
+        });
+
+        var quakeID = null;
+
+        map.on('mousemove', 'route-viz', e => {
+          map.getCanvas().style.cursor = 'pointer';
+
+          if (e.features.length > 0) {
+
+          	 var Rshield = '<img src="assets/img/shields/'+ e.features[0].properties.Route +'.png" alt="Highway Route Shield"/>';
+
+            var content = e.features[0].properties.Location
+            +' - '
+            +e.features[0].properties.County
+            +' County';
+            // Display the magnitude, location, and time in the sidebar
+            magDisplay.innerHTML = content;
+            shieldDisplay.innerHTML = Rshield;
+
+            // When the mouse moves over the earthquakes-viz layer, update the
+            // feature state for the feature under the mouse
+            if (quakeID) {
+              map.removeFeatureState({
+                source: 'route',
+                id: quakeID
+              });
+            }
+
+            quakeID = e.features[0].id;
+        
+            map.setFeatureState(
+              {
+                source: 'route',
+                id: quakeID
+              },
+              {
+                hover: true
+              }
+            );
+          }
+        });
+
+        // When the mouse leaves the earthquakes-viz layer, update the
+        // feature state of the previously hovered feature
+        map.on('mouseleave', 'route-viz', function() {
+          if (quakeID) {
+            map.setFeatureState(
+              {
+                source: 'route',
+                id: quakeID
+              },
+              {
+                hover: false
+              }
+            );
+          }
+          quakeID = null;
+   
+          // Remove the information from the previously hovered feature from the sidebar
+          magDisplay.innerHTML = '';
+          shieldDisplay.innerHTML ='Hover over an incident location for more information';
+          // Reset the cursor style
+          map.getCanvas().style.cursor = '';
+        });
+
+ 
+      
+    });
+
+
---
+++ assets/js/zoom.js
@@ -0,0 +1,288 @@
+// Zoom to look up
+   document.getElementById('PA').addEventListener('click', function () {
+        $("div h4:contains('Remove Filter')").hide();
+        map.flyTo({
+            center: [-75.4, 40.15], 
+            zoom: 8,
+            speed: 0.5
+        });
+
+    });
+
+    document.getElementById('BUC').addEventListener('click', function () {
+        $("div h4:contains('Remove Filter')").show();
+        map.flyTo({
+            center: [-75.106870407001679,40.336933676997319], 
+            zoom: 9,
+            speed: 0.5
+        });
+    });
+    document.getElementById('CHE').addEventListener('click', function () {
+      $("div h4:contains('Remove Filter')").show();
+      map.flyTo({
+          center: [-75.748451366853885,39.973209680949878], 
+          zoom: 9,
+          speed: 0.5
+      });
+    });
+      document.getElementById('DEL').addEventListener('click', function () {
+      $("div h4:contains('Remove Filter')").show();
+      map.flyTo({
+          center: [-75.398773932333029,39.916606269175929], 
+          zoom: 10,
+          speed: 0.5
+      });
+    });
+      document.getElementById('MON').addEventListener('click', function () {
+      $("div h4:contains('Remove Filter')").show();
+      map.flyTo({
+          center: [-75.367334983140267,40.210850123794955], 
+          zoom: 9,
+          speed: 0.5
+      });
+    });
+    document.getElementById('PHI').addEventListener('click', function () {
+      $("div h4:contains('Remove Filter')").show();
+      map.flyTo({
+          center: [-75.13417520195253,40.007663105527399], 
+          zoom: 10,
+          speed: 0.5
+      });
+    });
+// Bucks County
+    document.getElementById('BUC_I095').addEventListener('click', function () {
+        $("div h4:contains('Remove Filter')").show();
+      map.flyTo({
+          center: [-74.934651,40.083523],
+          zoom: 11,
+          speed: 0.5
+      });
+    });
+      document.getElementById('BUC_I295').addEventListener('click', function () {
+        $("div h4:contains('Remove Filter')").show();
+      map.flyTo({
+          center: [-74.879212,40.1930960001],
+          zoom: 11,
+          speed: 0.5
+      });
+    });
+      document.getElementById('BUC_US001').addEventListener('click', function () {
+        $("div h4:contains('Remove Filter')").show();
+      map.flyTo({
+          center: [-74.879212,40.1930960001],
+          zoom: 11,
+          speed: 0.5
+      });
+    });
+      document.getElementById('BUC_US013').addEventListener('click', function () {
+        $("div h4:contains('Remove Filter')").show();
+      map.flyTo({
+          center: [-74.8124830002,40.1738419999],
+          zoom: 12,
+          speed: 0.5
+      });
+    });
+    document.getElementById('BUC_US202').addEventListener('click', function () {
+        $("div h4:contains('Remove Filter')").show();
+      map.flyTo({
+          center: [-75.1243709995,40.2996050002],
+          zoom: 13,
+          speed: 0.5
+      });
+    });
+    document.getElementById('BUC_PA063').addEventListener('click', function () {
+        $("div h4:contains('Remove Filter')").show();
+      map.flyTo({
+          center: [-74.9631559998,40.0733509998],
+          zoom: 15,
+          speed: 0.5
+      });
+    });    
+    document.getElementById('BUC_PA309').addEventListener('click', function () {
+        $("div h4:contains('Remove Filter')").show();
+      map.flyTo({
+          center: [-75.3297936324,40.3532856544],
+          zoom: 11,
+          speed: 0.5
+      });
+    }); 
+    document.getElementById('BUC_PA611').addEventListener('click', function () {
+        $("div h4:contains('Remove Filter')").show();
+      map.flyTo({
+          center: [-75.142324,40.305509],
+          zoom: 12,
+          speed: 0.5
+      });
+    }); 
+// Chester
+      document.getElementById('CHE_US001').addEventListener('click', function () {
+        $("div h4:contains('Remove Filter')").show();
+      map.flyTo({
+          center: [-75.8766136156,39.8267931362],
+          zoom: 10,
+          speed: 0.5
+      });
+    });
+    document.getElementById('CHE_US030').addEventListener('click', function () {
+        $("div h4:contains('Remove Filter')").show();
+      map.flyTo({
+          center: [-75.7598402177,40.0061459498],
+          zoom: 10,
+          speed: 0.5
+      });
+    });
+    document.getElementById('CHE_US202').addEventListener('click', function () {
+        $("div h4:contains('Remove Filter')").show();
+      map.flyTo({
+          center: [-75.501410,40.017994],
+          zoom: 10,
+          speed: 0.5
+      });
+    });    
+     document.getElementById('CHE_US422').addEventListener('click', function () {
+        $("div h4:contains('Remove Filter')").show();
+      map.flyTo({
+          center: [-75.4858576858,40.1638485564],
+          zoom: 10,
+          speed: 0.5
+      });
+    });  
+    document.getElementById('CHE_PA100').addEventListener('click', function () {
+        $("div h4:contains('Remove Filter')").show();
+      map.flyTo({
+          center: [-75.6608889997,40.2303680002],
+          zoom: 12,
+          speed: 0.5
+      });
+    }); 
+// Delaware
+      document.getElementById('DEL_I095').addEventListener('click', function () {
+        $("div h4:contains('Remove Filter')").show();
+      map.flyTo({
+          center: [-75.344236,39.867284,],
+          zoom: 11,
+          speed: 0.5
+      });
+    });
+    document.getElementById('DEL_I476').addEventListener('click', function () {
+        $("div h4:contains('Remove Filter')").show();
+      map.flyTo({
+          center: [-75.3357888651,39.9683093676],
+          zoom: 10,
+          speed: 0.5
+      });
+    });
+    document.getElementById('DEL_US001').addEventListener('click', function () {
+        $("div h4:contains('Remove Filter')").show();
+      map.flyTo({
+          center: [-75.3811900453,39.9363903359],
+          zoom: 12,
+          speed: 0.5
+      });
+    });    
+    document.getElementById('DEL_US322').addEventListener('click', function () {
+        $("div h4:contains('Remove Filter')").show();
+      map.flyTo({
+          center:[-75.4158480004,39.843517],
+          zoom: 14,
+          speed: 0.5
+      });
+    });     
+// Montgomery
+      document.getElementById('MON_I076').addEventListener('click', function () {
+        $("div h4:contains('Remove Filter')").show();
+      map.flyTo({
+          center: [-75.3147700006,40.06606],
+          zoom: 11,
+          speed: 0.5
+      });
+    });
+    document.getElementById('MON_I476').addEventListener('click', function () {
+        $("div h4:contains('Remove Filter')").show();
+      map.flyTo({
+          center: [-75.3104238556,40.0849828479],
+          zoom: 12,
+          speed: 0.5
+      });
+    });
+    document.getElementById('MON_US202').addEventListener('click', function () {
+        $("div h4:contains('Remove Filter')").show();
+      map.flyTo({
+          center: [-75.400737999,40.0821630009],
+          zoom: 14,
+          speed: 0.5
+      });
+    });    
+    document.getElementById('MON_US422').addEventListener('click', function () {
+        $("div h4:contains('Remove Filter')").show();
+      map.flyTo({
+          center: [-75.4858576858,40.1638485564],
+          zoom: 10,
+          speed: 0.5
+      });
+    });  
+    document.getElementById('MON_PA100').addEventListener('click', function () {
+        $("div h4:contains('Remove Filter')").show();
+      map.flyTo({
+          center:[-75.6437210004,40.2883350004],
+          zoom: 11,
+          speed: 0.5
+      });
+    });      
+      document.getElementById('MON_PA309').addEventListener('click', function () {
+        $("div h4:contains('Remove Filter')").show();
+      map.flyTo({
+          center:[-75.2046551619,40.137165],
+          zoom: 11,
+          speed: 0.5
+      });
+    });  
+// PHILLY
+      document.getElementById('PHI_I076').addEventListener('click', function () {
+        $("div h4:contains('Remove Filter')").show();
+      map.flyTo({
+          center: [-75.1820350003,39.9610589997],
+          zoom: 11,
+          speed: 0.5
+      });
+    });
+    document.getElementById('PHI_I095').addEventListener('click', function () {
+        $("div h4:contains('Remove Filter')").show();
+      map.flyTo({
+          center: [-75.0957349995,39.984336],
+          zoom: 10,
+          speed: 0.5
+      });
+    });
+    document.getElementById('PHI_I676').addEventListener('click', function () {
+        $("div h4:contains('Remove Filter')").show();
+      map.flyTo({
+          center: [-75.1587904613,39.9573379444],
+          zoom: 13,
+          speed: 0.5
+      });
+    });    
+    document.getElementById('PHI_US001').addEventListener('click', function () {
+        $("div h4:contains('Remove Filter')").show();
+      map.flyTo({
+          center:[-75.1704750001,40.0179950001],
+          zoom: 13,
+          speed: 0.5
+      });
+    });  
+    document.getElementById('PHI_PA063').addEventListener('click', function () {
+        $("div h4:contains('Remove Filter')").show();
+      map.flyTo({
+          center:[-74.9779280004,40.0921539998],
+          zoom: 13,
+          speed: 0.5
+      });
+    });  
+      document.getElementById('PHI_Bridges').addEventListener('click', function () {
+        $("div h4:contains('Remove Filter')").show();
+      map.flyTo({
+          center: [-75.199120,39.930980],
+          zoom: 11,
+          speed: 0.5
+      });
+    });        
